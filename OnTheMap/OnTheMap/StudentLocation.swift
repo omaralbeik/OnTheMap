abd0fbb4cec3ada20793cfba57a2b411aa99393b
Omar Albeik <omaralbeik@gmail.com>
2015-09-20 20:25:10 +0300

    StudentLocation Model Added

---
+++ OnTheMap/OnTheMap/StudentLocation.swift
@@ -0,0 +1,51 @@
+//
+//  StudentLocation.swift
+//  OnTheMap
+//
+//  Created by Omar Albeik on 20/09/15.
+//  Copyright © 2015 Omar Albeik. All rights reserved.
+//
+
+import Foundation
+
+//TODO: import NSDate from String using NSDateFormatter
+
+var locations: [StudentLocation] = []
+
+struct StudentLocation {
+    
+    var createdAt: String?
+    var firstName: String?
+    var lastName: String?
+    var latitude: Double?
+    var longitude: Double?
+    var mapString: String?
+    var mediaURL: String?
+    var objectId: String?
+    var uniqueKey: String?
+    var updatedAt: String?
+    
+    init (dictionary: [String : AnyObject]) {
+        createdAt = dictionary["createdAt"] as? String
+        firstName = dictionary["firstName"] as? String
+        lastName = dictionary["lastName"] as? String
+        latitude = dictionary["latitude"] as? Double
+        longitude = dictionary["longitude"] as? Double
+        mapString = dictionary["mapString"] as? String
+        mediaURL = dictionary["mediaURL"] as? String
+        objectId = dictionary["objectId"] as? String
+        uniqueKey = dictionary["uniqueKey"] as? String
+        updatedAt = dictionary["updatedAt"] as? String
+    }
+    
+    // Helper: Given an array of dictionaries, convert them to an array of StudentLocation objects
+    static func locationsFromResults(results: [[String : AnyObject]]) -> [StudentLocation] {
+        var locations = [StudentLocation]()
+        
+        for result in results {
+            locations .append(StudentLocation(dictionary: result))
+        }
+        return locations
+    }
+    
+}
