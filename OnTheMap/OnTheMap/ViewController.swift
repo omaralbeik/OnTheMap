0f7ee2ed830fb713123754becc2990c9e657cef8
Omar Albeik <omaralbeik@gmail.com>
2015-09-20 16:55:12 +0300

    Created Project

---
+++ OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Omar Albeik on 20/09/15.
+//  Copyright © 2015 Omar Albeik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
